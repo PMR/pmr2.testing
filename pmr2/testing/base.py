43a82b495fceb422fc770d3df0f73fbb6bb6278d
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-01-16 15:04:39 +1300

    Removed the form specific test helpers.
      - Those helper classes have been moved to pmr2.z3cform, as that is the
        package dedicated to dealing with forms.
      - This package still provides certain helper classes and data that is
        needed to fully test PMR2 proper.

--- pmr2/testing/base.py
+++ pmr2/testing/base.py
@@ -5,12 +5,9 @@ import shutil
 import zope.interface
 import zope.component
 import z3c.form.testing
-from zope.app.publication.zopepublication import BeforeTraverseEvent
 from zope.annotation import IAnnotations
 from Zope2.App.zcml import load_config
 
-from plone.browserlayer.layer import mark_layer
-from plone.browserlayer.utils import register_layer
 from plone.keyring.interfaces import IKeyManager
 from plone.protect.authenticator import _getUserName
 from plone.protect.authenticator import sha
@@ -18,53 +15,9 @@ from plone.protect.authenticator import sha
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.CMFCore.utils import getToolByName
 
+from pmr2.z3cform.tests import base
+from pmr2.z3cform.tests.base import IPMR2TestRequest, TestRequest
 import pmr2.testing
-
-
-class IPMR2TestRequest(zope.interface.Interface):
-    """\
-    Marker for PMR2 test request
-    """
-
-
-class TestRequest(z3c.form.testing.TestRequest):
-    """\
-    Customized TestRequest to mimic missing actions.
-    """
-
-    # IAnnotations applied by plone.z3cform test case.
-    zope.interface.implements(IAnnotations, IPMR2TestRequest)
-    def __init__(self, *a, **kw):
-        super(TestRequest, self).__init__(*a, **kw)
-        self.environ = {}
-        if self.form:
-            self.method = 'POST'
-            self._set_authenticator()
-
-    def __setitem__(self, key, value):
-        self.form[key] = value
-
-    def __getitem__(self, key):
-        try:
-            return super(TestRequest, self).__getitem__(key)
-        except KeyError:
-            return self.form[key]
-
-    def _set_authenticator(self):
-        manager = zope.component.queryUtility(IKeyManager)
-        if not manager:
-            # Since the key manager is not installed, authenticator 
-            # should not be working anyway.
-            return
-        secret = manager.secret()
-        user = _getUserName()
-        auth = hmac.new(secret, user, sha).hexdigest()
-        self['_authenticator'] = auth
-
-    def getApplicationURL(self):
-        # XXX compatibility with the more strict redirection introduced
-        # with zope.publisher-3.12, http.redirect's untrusted attribute.
-        return 'http://nohost:80'
 
 
 class TestCase(ptc.PloneTestCase):
@@ -73,21 +26,16 @@ class TestCase(ptc.PloneTestCase):
     """
 
 
-class DocTestCase(ptc.FunctionalTestCase):
-    """\
-    For doctests.
+class DocTestCase(base.DocTestCase):
+    """
+    Provide shortcut to publish content and create new temporary
+    directory for testing.
     """
 
     def setUp(self):
         load_config('test.zcml', pmr2.testing)
         super(DocTestCase, self).setUp()
         self.tmpdir = tempfile.mkdtemp()
-        register_layer(IPMR2TestRequest, 'pmr2.app.testing')
-
-    def markLayer(self):
-        # Helper method to trigger the mark layer event.
-        event = BeforeTraverseEvent(self.portal, self.portal.REQUEST)
-        mark_layer(self.portal, event)
 
     def tearDown(self):
         super(DocTestCase, self).tearDown()
@@ -101,4 +49,3 @@ class DocTestCase(ptc.FunctionalTestCase):
         self.setRoles(('Manager',))
         pw.doActionFor(obj, "publish")
         self.setRoles(('Member', 'Authenticated',))
-
